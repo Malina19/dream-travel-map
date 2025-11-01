10b727bd1eb8dabe194c2c1dd93274e6c7af051f
Dawid Malik <dawid.malik.it@gmail,com$  git config --global user.email dawid.malik.it@gmail.com>
2025-11-01 21:32:27 +0100

    Configure for github pages deployment

--- dream-travel-map/vite.config.js
+++ dream-travel-map/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
+  base: '/dream-travel-map/', // 🆕 ZMIEŃ "dream-travel-map" na NAZWĘ TWOJEGO REPO!
 })
